2939472ba30415164929491c74d561dafe6bb263
dwaynek24 <devinwayneknotts@gmail.com>
2022-10-06 10:57:51 -0700

    COmplete Read me

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function AboutPage(props) {
-    return (
-        
-          
-          <p className= "about">Are you Bored? 
-            Let me fix that.
-             Activate me and I can suggest you things do from busy work,
-              to something relaxing at home.</p>
-             
-    );
+  return (
+    <p className="about">
+      Are you Bored? Let me fix that. Activate me and I can suggest you things
+      do from busy work, to something relaxing at home.
+    </p>
+  );
 }
 
 export default AboutPage;
